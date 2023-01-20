b5db3df8462481e7bf79f2498292fb7625f52e0d
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-01-20 12:51:20 -0500

    Update cart.js

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,16 +4,16 @@ function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   // const keys = Object.keys(cartItems);
   // const values = Object.values(cartItems);
-  const entries = Object.entries(cartItems);
-  console.log(entries);
+  //const entries = Object.entries(cartItems);
+  //console.log(entries);
   
-  entries.forEach(element => console.log(element) );
+  //entries.forEach(element => console.log(element) );
   //for (const key of keys) {console.log(key);}
   //for (const value of values) {console.log(value);} 
   // for(var entry of entries) {
   //   return entry = `${entry[0]}: ${entry[1]}`;}
 
-  const htmlItems = entries.map((item) => cartItemTemplate(item));
+  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
  
   //I think the problem is that the local storage is returning an object instead of an array and .map() can onoy be used on arrays.
