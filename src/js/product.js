4623959a761d3ce23a5e06e3c167bc7a32151931
Allidsr <agu20010@byui.edu>
2023-01-24 04:08:46 -0600

    assingment 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,12 @@
-import { setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
+import { setLocalStorage, getParam } from './utils.mjs';
 
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData('tents');
+const productId = getParam('product');
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
