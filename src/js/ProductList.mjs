4a4439d488b3ce1a8878aa19f139fc0d96c7917c
Allidsr <agu20010@byui.edu>
2023-01-26 11:04:23 -0600

    thursday meeting

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,6 +1,23 @@
+function productCardTemplate(product) {
+    return `<li class="product-card">
+    <a href="product_pages/index.html?product=">
+    <img
+      src=""
+      alt="Image of "
+    />
+    <h3 class="card__brand"></h3>
+    <h2 class="card__name"></h2>
+    <p class="product-card__price">$</p></a>
+  </li>`
+} 
+   
 export default class ProductList {
-constructor(category,dataSource){
+constructor(category, dataSource, listElement){
 this.category = category;
 this.dataSource = dataSource;
+this.listElement = listElement;
 }
+async init() {
+    const list = await this.dataSource.getData();
+  }
 }
