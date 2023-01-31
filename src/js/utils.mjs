81075503390bd34d6e88a9c59328f28465c65f4c
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-01-31 12:11:54 -0500

    week 5 team meeting

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -14,13 +14,7 @@ export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
 // set a listener for both touchend and click
-export function setClick(selector, callback) {
-  qs(selector).addEventListener("touchend", (event) => {
-    event.preventDefault();
-    callback();
-  });
-  qs(selector).addEventListener("click", callback);
-}
+
 
 export function getParam(param) {
   const queryString = window.location.search;  
@@ -28,4 +22,37 @@ export function getParam(param) {
   const product = urlParams.get(param)
 
   return product
+}
+
+//pasted from productDetails
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.insertAdjacentHTML("afterbegin", template);
+  if(callback) {
+    callback(data);
+  }
+  
+}
+
+export async function loadTemplate(path){
+  const response = await fetch(path);
+  const template = await response.text();
+  return template;
+}
+
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const headerElement = document.querySelector('#mainHeader');
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+  const footerElement = document.querySelector('#mainFooter');
+
+renderWithTemplate(headerTemplate, headerElement);
+renderWithTemplate(footerTemplate, footerElement);
+}
+
+export function setClick(selector, callback) {
+  qs(selector).addEventListener("touchend", (event) => {
+    event.preventDefault();
+    callback();
+  });
+  qs(selector).addEventListener("click", callback);
 }
